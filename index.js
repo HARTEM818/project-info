3254c56f3996d24d2f85fbdb1aac6394fbd3f394
HARTEM818 <artemhavryliuk777@gmail.com>
2025-04-10 10:58:44 +0300

    js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const headerContainer  = document.querySelector(".header__container")
-burgerMenu.addEventListener("click", () =>{
+burgerMenu.addEventListener("click", () => {
     headerContainer.classList.toggle("header__max-height")
 })
