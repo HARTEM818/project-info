60de5eb32c71d0eb7db5c6e20cdf3b0f0e89e4ca
HARTEM818 <artemhavryliuk777@gmail.com>
2025-05-02 21:45:14 +0300

    first js update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const burgerMenu = document.querySelector(".burger-menu");
-const headerContainer  = document.querySelector(".header__container")
+const headerContainer = document.querySelector(".header__container");
 burgerMenu.addEventListener("click", () => {
-    headerContainer.classList.toggle("header__max-height")
-})
-// const categoryList = document.querySelector(".category__list");
-// const categorySquare = document.querySelectorAll(".category__square");
-// categoryList.addEventListener("click", () => {
-//     
-// })
+  headerContainer.classList.toggle("header__max-height");
+});
 
 
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    card.classList.add("card__shadow")
+    document.querySelectorAll(".category__square").forEach((square) => {
+      square.classList.add("disable");
+    });
+    const square = card.querySelector(".category__square");
+    card.classList.remove("card__shadow")
+    square.classList.remove("disable");
+  });
+});
