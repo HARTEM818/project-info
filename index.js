f3a0387754274bea26505ce370812ee5c088c966
HARTEM818 <artemhavryliuk777@gmail.com>
2025-05-03 21:01:31 +0300

    modal-window

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const headerContainer = document.querySelector(".header__container");
+const body = document.body;
 burgerMenu.addEventListener("click", () => {
   headerContainer.classList.toggle("header__max-height");
+  body.classList.toggle("no-scroll");
 });
+
+const a = document.querySelector(" .hero__interaction-btn + div > a")
+const modal = document.querySelector(".modal-window")
+a.addEventListener("click", (e) => {
+  e.preventDefault()
+  modal.style.display = "block"
+  body.classList.toggle("no-scroll");
+  headerContainer.style.display = "none"
+})
+
 
 
 const cards = document.querySelectorAll(".card");
