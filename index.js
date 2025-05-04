ba858b917ade31f5668e9d6652443064ab2a7c8b
HARTEM818 <artemhavryliuk777@gmail.com>
2025-05-04 10:40:26 +0300

    modal upd

--- index.js
+++ index.js
@@ -6,27 +6,41 @@ burgerMenu.addEventListener("click", () => {
   body.classList.toggle("no-scroll");
 });
 
-const a = document.querySelector(" .hero__interaction-btn + div > a")
-const modal = document.querySelector(".modal-window")
-a.addEventListener("click", (e) => {
-  e.preventDefault()
-  modal.style.display = "block"
-  body.classList.toggle("no-scroll");
-  headerContainer.style.display = "none"
-})
+if (window.matchMedia("(min-width: 1440px)").matches) {
+  const a = document.querySelector(".hero__interaction-btn + div > a");
+  const modalOverlay = document.getElementById("modalOverlay");
+  const modalWindow = document.getElementById("modalWindow");
+  const body = document.body;
+  const headerContainer = document.querySelector("header");
 
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+    modalOverlay.style.display = "flex";
+    body.classList.toggle("no-scroll");
+    headerContainer.style.display = "none";
+  });
 
+  modalOverlay.addEventListener("click", () => {
+    modalOverlay.style.display = "none";
+    body.classList.toggle("no-scroll");
+    headerContainer.style.display = "block";
+  });
+
+  modalWindow.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+}
 
 const cards = document.querySelectorAll(".card");
 
 cards.forEach((card, index) => {
   card.addEventListener("click", () => {
-    card.classList.add("card__shadow")
+    card.classList.add("card__shadow");
     document.querySelectorAll(".category__square").forEach((square) => {
       square.classList.add("disable");
     });
     const square = card.querySelector(".category__square");
-    card.classList.remove("card__shadow")
+    card.classList.remove("card__shadow");
     square.classList.remove("disable");
   });
 });
